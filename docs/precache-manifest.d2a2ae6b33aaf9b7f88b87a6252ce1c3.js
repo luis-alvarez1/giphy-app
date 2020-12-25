0f26b42213b3d86c3ed43c5273d400d8274663e6
Luis Alvarez <lealvarezp@unac.edu.co>
2020-12-25 03:53:39 -0500

    docs

---
+++ docs/precache-manifest.d2a2ae6b33aaf9b7f88b87a6252ce1c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5952f6f7d82b382affa5760044499794",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5c902780e0a13548255",
+    "url": "/static/css/main.a876c27c.chunk.css"
+  },
+  {
+    "revision": "f6104528819a19072ec9",
+    "url": "/static/js/2.9e57f6ae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9e57f6ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5c902780e0a13548255",
+    "url": "/static/js/main.071d3919.chunk.js"
+  },
+  {
+    "revision": "e775e7a58d8a9a5e5140",
+    "url": "/static/js/runtime-main.d76e4de2.js"
+  }
+]);
